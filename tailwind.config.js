931904aca9b4793bf7486dc8055eaef01bcf5c28
Princeton Aihebho <letstalk@cofellow.com>
2025-08-30 12:06:28 +0100

    Update favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-const config = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        brand: "rgb(0, 165, 184)", // or '#00A5B8'
-      },
-    },
-  },
-  plugins: [],
-};
+import tailwindcss from "tailwindcss";
+// import autoprefixer from "autoprefixer";
 
-export default config;
+export default {
+  plugins: {
+    tailwindcss,
+    autoprefixer,
+  },
+};
